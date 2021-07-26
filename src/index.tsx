4aa507095fed08181b4d74647d4dca6f88f8a366
Patrick Perosa <patrickperosapp@gmail.com>
2021-07-26 18:39:04 -0400

    feat:edit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom'
-import { MiniEcommerce } from './pages/MiniEcommerce'
-import { Upload } from './pages/Upload'
 
-ReactDOM.render(<Upload />, document.getElementById('root'))
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import { TaskList } from '../components/TaskList'
-import { TaskRegister } from '../components/TaskRegister'
-import { TaskUpdate } from '../components/TaskUpdate'
+import { TaskList } from 'components/TaskList'
+import { TaskRegister } from 'components/TaskRegister'
+import { TaskUpdate } from 'components/TaskUpdate'
+import { MiniEcommerce } from 'pages/MiniEcommerce'
+import { Upload } from 'pages/Upload'
+import Calculator from 'pages/Calculator/Calculator'
 
 const Routes = () => {
   return (
@@ -11,6 +14,9 @@ const Routes = () => {
       <Route path="/" exact component={TaskList} />
       <Route path="/register" exact component={TaskRegister} />
       <Route path="/update/:id" exact component={TaskUpdate} />
+      <Route path="/ecommerce" exact component={MiniEcommerce} />
+      <Route path="/upload" exact component={Upload} />
+      <Route path="/calculator" exact component={Calculator} />
     </Switch>
   )
 }
